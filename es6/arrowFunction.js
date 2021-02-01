0ba196ce7aa3f8023e2adf35e2db44176d2c725e
Lucas Henrique <luketevl@gmail.com>
2021-02-01 14:43:33 -0300

    Update arrowFunction.js

--- es6/arrowFunction.js
+++ es6/arrowFunction.js
@@ -10,6 +10,6 @@ const names = peoples.map( people => people.name )
 
 
 // old
-const names = peoples.map( fuction(people) {
+const namesFn = peoples.map( fuction(people) {
   return  people.name;
 })
